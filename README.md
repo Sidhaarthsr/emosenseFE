b3ee9f1808f8f9d8fbb9c187e078c4d4ba82b3ba
sanketshinde07 <sanketshinde0717@gmail.com>
2024-03-17 12:50:05 -0400

    initial commit. Added card component, image save feature, color change based on arbitary value

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# emosenseFE
-FrontEnd Repository for Emosense, an AI-powered Dynamic UI to reduce eye strain.
+## FrontEnd Repository for Emosense, an AI-powered Dynamic UI to reduce eye strain.
+
+```
+Step 1: Ensure node is installed on your system. 
+Step 2: If Vue js 2 is not installed,Execute "npm install vue@2" to install it
+Step 3: Once successfully installed, Open codebase in visual studio code
+Step 4: Open terminal in Visual Studio code, and execute "npm install". This will install all the required modules
+Step 5: Execute "npm run serve" to run the code
+
+```
+
+
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="card">
+        <div class="card-top" :style="{ backgroundColor: color }"></div> <!-- Apply background color dynamically -->
+      <div class="card-header">{{ header }}</div>
+      <div class="card-body">
+        <p>{{ body }}</p>
+      </div>
+      <div class="card-footer">{{ footer }}</div>
+    </div>
+  </template>
+
+  
+  <script>
+  export default {
+    name: 'CardComponent',
+    props: {
+      header: {
+        type: String,
+        default: 'Default Header'
+      },
+      body: {
+        type: String,
+        default: 'Default Body'
+      },
+      footer: {
+        type: String,
+        default: 'Default Footer'
+      },
+      color: {
+      type: String,
+      default: 'red' // Default color if not provided
+    }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .card {
+    border: 1px solid #ccc;
+    border-radius: 8px;
+    padding: 16px;
+    margin-bottom: 16px;
+  }
+  
+  .card-header {
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 12px;
+  }
+  
+  .card-body {
+    margin-bottom: 12px;
+  }
+
+  .card-top{
+    height: 25px; /* Adjust height as needed */
+  border-radius: 8px 8px 0 0; /* Apply border-radius to top corners */
+  }
+  
+  .card-footer {
+    font-style: italic;
+    color: #666;
+  }
+  </style>
+  
